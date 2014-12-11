fbffa127b1861ece3bc3626996f0011f3458aefa
Turupawn <ahmed.hn.43@gmail.com>
2014-12-11 11:28:42 -0600

    test

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "Lista.h"
+#include "ArbolTrinario.h"
+#include "Evaluador.h"
+#include <fstream>
+#include <list>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+//1 a realizar estan ubicados en el archivo Lista.h
+
+float obtenerPromedio(std::list<float> mi_lista)
+{
+    return 0;
+}
+
+int obtenerMediana(std::vector<int> mi_vector)
+{
+    return 0;
+}
+
+bool existeEnArchivo(string nombre_archivo, string cadena)
+{
+    return 0;
+}
+
+char obtenerUltimaLetra(string nombre_archivo)
+{
+    return 0;
+}
+
+int contarEnArbol(NodoTrinario* raiz, int num)
+{
+    return 0;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
+
